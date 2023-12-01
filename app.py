176b2c4a742e3ebc34a94afbbb90f7be85fc73dd
Bilal Ahmad <bilal.codemaster@gmail.com>
2023-12-01 06:07:20 -0800

    Roll No:2091,2092,2093,2102

--- app.py
+++ app.py
@@ -1,6 +1,60 @@
+
+# import tkinter as tk
+
+# # root.title('Malik')
+
+
+
+
+
+# # def generate_fee_voucher():
+# #     student_name = entry_name.get()
+# #     course_fee = entry_course_fee.get()
+
+# #     # Validate input
+# #     if not student_name or not course_fee.isdigit():
+# #         result_label.config(text="Invalid input. Please enter valid data.")
+# #         return
+
+# #     total_fee = int(course_fee)
+
+# #     # Create fee voucher text
+# #     voucher_text = f"Student Name: {student_name}\nCourse Fee: {course_fee} Rs.\nTotal Fee: {total_fee} Rs."
+
+# #     # Display the voucher
+# #     result_label.config(text=voucher_text)
+
+# # # Create the main window
+# # root.title("Fee Voucher Generator")
+
+# # # Create and place widgets
+# # label_name = tk.Label(root, text="Student Name:")
+# # label_name.grid(row=0, column=0, padx=10, pady=10, sticky=tk.W)
+
+# # entry_name = tk.Entry(root)
+# # entry_name.grid(row=0, column=1, padx=10, pady=10)
+
+# # label_course_fee = tk.Label(root, text="Course Fee:")
+# # label_course_fee.grid(row=1, column=0, padx=10, pady=10, sticky=tk.W)
+
+# # entry_course_fee = tk.Entry(root)
+# # entry_course_fee.grid(row=1, column=1, padx=10, pady=10)
+
+# # generate_button = tk.Button(root, text="Generate Voucher", command=generate_fee_voucher)
+# # generate_button.grid(row=2, column=0, columnspan=2, pady=10)
+
+# # result_label = tk.Label(root, text="")
+# # result_label.grid(row=3, column=0, columnspan=2, pady=10)
+
+
+
+# root.mainloop()
+
+
 import tkinter as tk
 
-def file_new():
+
+def Admition_Voucher():
   
 
 
@@ -26,31 +80,39 @@ def file_new():
 
     # Create and place widgets
     label_name = tk.Label(root, text="Student Name:")
-    label_name.grid(row=0, column=0, padx=10, pady=10, sticky=tk.W)
+    label_name.grid(row=1, column=0, padx=10, pady=10, sticky=tk.W)
 
     entry_name = tk.Entry(root)
-    entry_name.grid(row=0, column=1, padx=10, pady=10)
+    entry_name.grid(row=1, column=1, padx=10, pady=10)
 
     label_course_fee = tk.Label(root, text="Course Fee:")
-    label_course_fee.grid(row=1, column=0, padx=10, pady=10, sticky=tk.W)
+    label_course_fee.grid(row=2, column=0, padx=10, pady=10, sticky=tk.W)
 
     entry_course_fee = tk.Entry(root)
-    entry_course_fee.grid(row=1, column=1, padx=10, pady=10)
+    entry_course_fee.grid(row=2, column=1, padx=10, pady=10)
 
     generate_button = tk.Button(root, text="Generate Voucher", command=generate_fee_voucher)
-    generate_button.grid(row=2, column=0, columnspan=2, pady=10)
+    generate_button.grid(row=3, column=0, columnspan=2, pady=10)
 
     result_label = tk.Label(root, text="")
-    result_label.grid(row=3, column=0, columnspan=2, pady=10)
+    result_label.grid(row=4, column=0, columnspan=2, pady=10)
 
 
 
-def file_open():
+def Semester_Voucher():
     
 
+
     def generate_fee_voucher():
+
+        def   print_fee_voucher():
+            out=tk.Label(root,text="Printed").grid(row=9,column=0, columnspan=2, pady=10)
+
         student_name = entry_name.get()
         course_fee = entry_course_fee.get()
+        generate_button = tk.Button(root, text="Print Voucher", command=print_fee_voucher)
+        generate_button.grid(row=8, column=0, columnspan=2, pady=10)
+
 
         # Validate input
         if not student_name or not course_fee.isdigit():
@@ -70,65 +132,68 @@ def file_open():
 
     # Create and place widgets
     label_name = tk.Label(root, text="Student Name:")
-    label_name.grid(row=0, column=0, padx=10, pady=10, sticky=tk.W)
+    label_name.grid(row=1, column=0, padx=10, pady=10, sticky=tk.W)
 
     entry_name = tk.Entry(root)
-    entry_name.grid(row=0, column=1, padx=10, pady=10)
+    entry_name.grid(row=1, column=1, padx=10, pady=10)
 
     label_course_fee = tk.Label(root, text="Course Fee:")
-    label_course_fee.grid(row=1, column=0, padx=10, pady=10, sticky=tk.W)
+    label_course_fee.grid(row=2, column=0, padx=10, pady=10, sticky=tk.W)
 
     entry_course_fee = tk.Entry(root)
-    entry_course_fee.grid(row=1, column=1, padx=10, pady=10)
+    entry_course_fee.grid(row=2, column=1, padx=10, pady=10)
 
     generate_button = tk.Button(root, text="Generate Voucher", command=generate_fee_voucher)
-    generate_button.grid(row=2, column=0, columnspan=2, pady=10)
+    generate_button.grid(row=3, column=0, columnspan=2, pady=10)
 
     result_label = tk.Label(root, text="")
-    result_label.grid(row=3, column=0, columnspan=2, pady=10)
+    result_label.grid(row=4, column=0, columnspan=2, pady=10)
 
 
-def file_save():
+
+def Repeat_course():
     
 
     def generate_fee_voucher():
-        student_name = entry_name.get()
-        course_fee = entry_course_fee.get()
+        
+        student_name = entry_student_name.get()
+        course = entry_course.get()
+        amount = entry_amount.get()
 
-        # Validate input
-        if not student_name or not course_fee.isdigit():
-            result_label.config(text="Invalid input. Please enter valid data.")
-            return
+        # Assuming a simple printout for demonstration purposes
+        voucher_text = f"Fee Voucher\n\nStudent Name: {student_name}\nCourse: {course}\nAmount: {amount}Rs."
 
-        total_fee = int(course_fee)
-
-        # Create fee voucher text
-        voucher_text = f"Student Name: {student_name}\nCourse Fee: {course_fee} Rs.\nTotal Fee: {total_fee} Rs."
-
-        # Display the voucher
-        result_label.config(text=voucher_text)
+        # Display the fee voucher in a new window
+        voucher_window = tk.Toplevel(root)
+        voucher_window.title("Fee Voucher")
+        voucher_label = tk.Label(voucher_window, text=voucher_text, padx=20, pady=20)
+        voucher_label.pack()
+        
+        
 
     # Create the main window
+    root = tk.Tk()
     root.title("Fee Voucher Generator")
 
-    # Create and place widgets
-    label_name = tk.Label(root, text="Student Name:")
-    label_name.grid(row=0, column=0, padx=10, pady=10, sticky=tk.W)
+    # Create labels and entry widgets
+    label_student_name = tk.Label(root, text="Student Name:")
+    label_student_name.grid(row=0, column=0, padx=10, pady=10, sticky="e")
+    entry_student_name = tk.Entry(root)
+    entry_student_name.grid(row=0, column=1, padx=10, pady=10)
 
-    entry_name = tk.Entry(root)
-    entry_name.grid(row=0, column=1, padx=10, pady=10)
+    label_course = tk.Label(root, text="Course:")
+    label_course.grid(row=1, column=0, padx=10, pady=10, sticky="e")
+    entry_course = tk.Entry(root)
+    entry_course.grid(row=1, column=1, padx=10, pady=10)
 
-    label_course_fee = tk.Label(root, text="Course Fee:")
-    label_course_fee.grid(row=1, column=0, padx=10, pady=10, sticky=tk.W)
+    label_amount = tk.Label(root, text="Amount:")
+    label_amount.grid(row=2, column=0, padx=10, pady=10, sticky="e")
+    entry_amount = tk.Entry(root)
+    entry_amount.grid(row=2, column=1, padx=10, pady=10)
 
-    entry_course_fee = tk.Entry(root)
-    entry_course_fee.grid(row=1, column=1, padx=10, pady=10)
-
+    # Create a button to generate the fee voucher
     generate_button = tk.Button(root, text="Generate Voucher", command=generate_fee_voucher)
-    generate_button.grid(row=2, column=0, columnspan=2, pady=10)
-
-    result_label = tk.Label(root, text="")
-    result_label.grid(row=3, column=0, columnspan=2, pady=10)
+    generate_button.grid(row=3, column=0, columnspan=2, pady=20)
 
 
 def edit_cut():
@@ -142,18 +207,39 @@ def edit_paste():
 
 # Create the main window
 root = tk.Tk()
-root.title("Tkinter Menu Example")
+root.title("Fee Voucher for IUB")
+root.iconbitmap('apple.ico')
+root.geometry('500x500')
+# root.config(background='gray')
 
 # Create a menu bar
 menu_bar = tk.Menu(root)
 root.config(menu=menu_bar)
 
+# main page 
+
+l=tk.Label(root, text='WELCOME TO IUB PORTAL FOR FEE VOUCHER',).grid(row=0,column=1,padx=10,pady=10)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # Create File menu
 file_menu = tk.Menu(menu_bar, tearoff=0)
 menu_bar.add_cascade(label="File", menu=file_menu)
-file_menu.add_command(label="Admition Voucher", command=file_new)
-file_menu.add_command(label="Semester voucher", command=file_open)
-file_menu.add_command(label="Rapeat Course Voucher", command=file_save)
+file_menu.add_command(label="Admition Voucher", command=Admition_Voucher)
+file_menu.add_command(label="Semester voucher", command=Semester_Voucher)
+file_menu.add_command(label="Rapeat Course Voucher", command=Repeat_course)
 file_menu.add_separator()
 file_menu.add_command(label="Exit", command=root.destroy)
 
@@ -165,4 +251,8 @@ edit_menu.add_command(label="Copy", command=edit_copy)
 edit_menu.add_command(label="Paste", command=edit_paste)
 
 # Run the main loop
+
+
+
+
 root.mainloop()
--- fee_voucher_custum_fee.py
+++ fee_voucher_custum_fee.py
@@ -1,21 +1,16 @@
 import tkinter as tk
+from tkinter import messagebox
 
-def generate_fee_voucher():
+def generate_voucher():
     student_name = entry_name.get()
-    course_fee = entry_course_fee.get()
+    student_roll = entry_roll.get()
+    fee_amount = entry_fee.get()
 
-    # Validate input
-    if not student_name or not course_fee.isdigit():
-        result_label.config(text="Invalid input. Please enter valid data.")
-        return
-
-    total_fee = int(course_fee)
-
-    # Create fee voucher text
-    voucher_text = f"Student Name: {student_name}\nCourse Fee: {course_fee} Rs.\nTotal Fee: {total_fee} Rs."
-
-    # Display the voucher
-    result_label.config(text=voucher_text)
+    if not student_name or not student_roll or not fee_amount:
+        messagebox.showerror("Error", "Please fill in all fields.")
+    else:
+        voucher_text = f"Fee Voucher\n\nStudent Name: {student_name}\nRoll Number: {student_roll}\nFee Amount: {fee_amount}"
+        voucher_label.config(text=voucher_text)
 
 # Create the main window
 root = tk.Tk()
@@ -23,22 +18,25 @@ root.title("Fee Voucher Generator")
 
 # Create and place widgets
 label_name = tk.Label(root, text="Student Name:")
-label_name.grid(row=0, column=0, padx=10, pady=10, sticky=tk.W)
-
+label_name.grid(row=0, column=0, sticky="e", padx=10, pady=5)
 entry_name = tk.Entry(root)
-entry_name.grid(row=0, column=1, padx=10, pady=10)
+entry_name.grid(row=0, column=1, padx=10, pady=5)
 
-label_course_fee = tk.Label(root, text="Course Fee:")
-label_course_fee.grid(row=1, column=0, padx=10, pady=10, sticky=tk.W)
+label_roll = tk.Label(root, text="Roll Number:")
+label_roll.grid(row=1, column=0, sticky="e", padx=10, pady=5)
+entry_roll = tk.Entry(root)
+entry_roll.grid(row=1, column=1, padx=10, pady=5)
 
-entry_course_fee = tk.Entry(root)
-entry_course_fee.grid(row=1, column=1, padx=10, pady=10)
+label_fee = tk.Label(root, text="Fee Amount:")
+label_fee.grid(row=2, column=0, sticky="e", padx=10, pady=5)
+entry_fee = tk.Entry(root)
+entry_fee.grid(row=2, column=1, padx=10, pady=5)
 
-generate_button = tk.Button(root, text="Generate Voucher", command=generate_fee_voucher)
-generate_button.grid(row=2, column=0, columnspan=2, pady=10)
+generate_button = tk.Button(root, text="Generate Voucher", command=generate_voucher)
+generate_button.grid(row=3, column=0, columnspan=2, pady=10)
 
-result_label = tk.Label(root, text="")
-result_label.grid(row=3, column=0, columnspan=2, pady=10)
+voucher_label = tk.Label(root, text="", font=("Arial", 12))
+voucher_label.grid(row=4, column=0, columnspan=2, pady=10)
 
-# Start the Tkinter event loop
+# Run the main loop
 root.mainloop()
